3694b751e1ceaf553bbd73bb017a089b2fc1dc08
Hafsa Raja <hrranger555@gmail.com>
2025-10-21 21:54:22 +0500

    Add files via upload

---
+++ src/pages/FeaturesPage.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Features from '../components/Features';
+
+const FeaturesPage: React.FC = () => {
+  return (
+    <div className="pt-16">
+      <Features />
+    </div>
+  );
+};
+
+export default FeaturesPage;
